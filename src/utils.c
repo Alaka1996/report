a8b1b3100907a1353efd5084135d3753d16818cc
Alaka1996 <bgalakananda@gmail.com>
2025-01-27 11:55:25 +0530

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,8 @@
 #include "utils.h"
 #include <stdio.h>
 
+#define BUFFER_SIZE 10
+
 int calculate_average(uint16_t *data, int size) {
     int sum = 0;
     for (int i = 0; i < size; i++) {
@@ -16,12 +18,13 @@ void print_data(uint16_t *data, int size) {
 }
 
 void process_data(uint16_t *data) {
+    uint16_t filtered_data[BUFFER_SIZE];
     int avg = calculate_average(data, BUFFER_SIZE);
     printf("Average sensor value: %d\n", avg);
 
     if (avg > 512) {
         printf("Warning: Sensor value exceeds threshold!\n");
-    } else {
+    } else if (avg < 512) {
         printf("Sensor value is within safe range.\n");
     }
 }
