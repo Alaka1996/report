452b895d01b840859c1cb44db09835b8f451417b
Alaka1996 <bgalakananda@gmail.com>
2025-01-27 11:54:42 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,18 +1,22 @@
-#include "sensor.h"
-#include "utils.h"
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include "sensor.h"
+#include "utils.h"
 
-void main() {
-    uint16_t *sensor_data = (uint16_t *)malloc(BUFFER_SIZE * sizeof(uint16_t));
+#define BUFFER_SIZE 10
+
+int main() {
+    uint16_t *sensor_data = (uint16_t *)malloc(BUFFER_SIZE);  
     if (!sensor_data) {
         printf("Memory allocation failed!\n");
-        return;
+        return 1;
     }
 
     read_sensor_data(sensor_data);
     process_data(sensor_data);
     print_data(sensor_data, BUFFER_SIZE);
 
-    free(sensor_data);
+    free(sensor_data);  // Free allocated memory
+    return 0;
 }
