6d9363a96de887b65ec36564db1d5dc31935368d
Alaka1996 <bgalakananda@gmail.com>
2025-01-27 15:00:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #define BUFFER_SIZE 10
 
 int main() {
-    uint16_t *sensor_data = (uint16_t *)malloc(BUFFER_SIZE );
+    uint16_t *sensor_data = (uint16_t *)malloc(BUFFER_SIZE * sizeof(uint16_t) );
     if (!sensor_data) {
         printf("Memory allocation failed!\n");
         return 1;
