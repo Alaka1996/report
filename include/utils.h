7dcf2536ca6563f6b74998da3f0e3aacbf5a2e29
Alaka1996 <bgalakananda@gmail.com>
2025-01-27 11:54:03 +0530

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,10 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <stdint.h>
-
 int calculate_average(uint16_t *data, int size);
 void print_data(uint16_t *data, int size);
 void process_data(uint16_t *data);
 
-#endif
+#endif // UTILS_H
