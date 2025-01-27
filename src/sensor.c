dd8722f1dc7c3256c605986da2263f2b48fb1370
Alaka1996 <bgalakananda@gmail.com>
2025-01-27 12:32:22 +0530

    Update sensor.c

--- src/sensor.c
+++ src/sensor.c
@@ -4,7 +4,7 @@
 #define BUFFER_SIZE 10
 
 void read_sensor_data(uint16_t *data) {
-    for (int i = 0; i < BUFFER_SIZE; i++) {  // Fixed: Changed to i < BUFFER_SIZE
+    for (int i = 0; i <= BUFFER_SIZE; i++) {  // Fixed: Changed to i < BUFFER_SIZE
         data[i] = rand() % 1024;
     }
 }
